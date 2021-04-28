dfa9b13cd2ee7a04e3a2d0651833aa2094dbb577
nh <Nadja.Hagen@novatec-gmbh.de>
2021-04-28 14:45:52 +0200

    Added Testing using Spock and Testcontainer

--- src/main/java/competingconsumers/version091/Producer.java
+++ src/main/java/competingconsumers/version091/Producer.java
@@ -1,44 +1,46 @@
 package competingconsumers.version091;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.MessageProperties;
+import com.rabbitmq.client.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
 public class Producer {
-
     private static final String TASK_QUEUE_NAME = "task_queue";
+    private Channel channel;
+    private ConnectionFactory factory;
+    private Connection connection;
 
     /**
-     * Waits for input from the console and sends it as a message to the queue.
-     * @param args
-     * @throws Exception
+     * Establishes a new connection to a RabbitMQ Broker which runs locally. Declares a new channel and queue.
+     * @param port port number of the Broker to connect to
      */
-    public static void main(String[] args) throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+    public Producer(int port) {
+        try {
+            factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            factory.setPort(port);
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+            channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        }catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
 
-            //in order to enable durability, the queue and the messages need to be marked as persistent
-            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
-
-            //wait for input from the console
-            while(true) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                System.out.println("Enter a message. The number of dots contained denotes the complexity:");
-                String message = br.readLine();
-
-                //send message to the queue and mark them as persistent
-                channel.basicPublish("", TASK_QUEUE_NAME,
-                        MessageProperties.PERSISTENT_TEXT_PLAIN,
-                        message.getBytes("UTF-8"));
-                System.out.println("Sent '" + message + "'");
-            }
+    /**
+     * Publishs a given String to the queue. The default exchange is used.
+     * @param message which should be sent
+     */
+    public void sendMessage(String message) {
+        try {
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                    MessageProperties.TEXT_PLAIN,
+                    message.getBytes(StandardCharsets.UTF_8));
+            System.out.println("Sent '" + message + "'");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
-
---
+++ src/main/java/competingconsumers/version091/ProducerLauncher.java
@@ -0,0 +1,31 @@
+package competingconsumers.version091;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ProducerLauncher {
+    private static final int RABBIT_MQ_PORT = 5672;
+
+    /**
+     * Enables to run the Producer from the command line.
+     * A local RabbitMQ broker instance needs to be started beforehand!
+     * Input is taken from the command line and is then forwarded for publishing.
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        Producer producer = new Producer(RABBIT_MQ_PORT);
+
+        //wait for input from the console
+        while (true) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter a message. The number of dots contained denotes the complexity. 'Exit' will stop the program.");
+            String message = br.readLine();
+            if(message.equals("Exit")) {
+                System.out.println("Program will be stopped.");
+                break;
+            }
+            producer.sendMessage(message);
+        }
+    }
+}
+
